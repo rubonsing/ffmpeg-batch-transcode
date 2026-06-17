dbe6abcca19e7d7d2c660f2442b4b05750b2d4e2
rubonsing <130290156+rubonsing@users.noreply.github.com>
2026-06-17 23:16:10 +0800

    Update recode_svt_low.py

--- recode_svt_low.py
+++ recode_svt_low.py
@@ -7,26 +7,10 @@ import os
 input_dir = "input"
 output_dir = "output"
 
-def run_test(ref, dis):
-    cmd_head = "ffmpeg -hide_banner -i " + ref + " -i " + dis + \
-        """ -lavfi "[0:v]settb=AVTB,setpts=PTS-STARTPTS[ref];\
-        [1:v]settb=AVTB,setpts=PTS-STARTPTS[dis];[ref][dis]"""
-    
-    cmd1 = cmd_head + """libvmaf=n_threads=14:shortest=true:model=version=vmaf_4k_v0.6.1"  -f null -"""
-    cmd2 = cmd_head + """ssim=shortest=true"  -f null -"""
-    cmd3 = cmd_head + """psnr=shortest=true"  -f null -"""
-    cmd4 = cmd_head + """xpsnr=shortest=true"  -f null -"""
-    subprocess.run(cmd1, check=True)
-    subprocess.run(cmd2, check=True)
-    subprocess.run(cmd3, check=True)
-    subprocess.run(cmd4, check=True)
-
 for filename in os.listdir(input_dir):
     input_path = os.path.join(input_dir, filename)
     output_path = os.path.join(output_dir, os.path.splitext(filename)[0] + ".mp4")
-
-    #output_path = "output/test.mp4"
-    print(f"正在转码：{input_path} → {output_path}")
+    print(f"\ntranscoding:{input_path} → {output_path}\n")
     cmd = [
         "ffmpeg", "-hide_banner", "-i", input_path,
         #"-ss", "00:00:00",
@@ -34,12 +18,11 @@ for filename in os.listdir(input_dir):
         #"-vf", "zscale=w=1728:h=1080:filter=lanczos,fps=30",
         "-c:v", "libsvtav1",
         "-svtav1-params", "preset=8:profile=0:tune=2:crf=40:keyint=600",
-        "-map", "0",            #映射所有轨道
-        "-map", "-0:d",         #删除数据轨道
-        "-c:a:1", "copy",       #复制音轨1，舍弃其他音轨
+        "-map", "0",
+        "-map", "-0:d",
+        "-c:a:1", "copy",
         output_path
     ]
     subprocess.run(cmd, check=True)
-    #run_test(input_path, output_path)
 
-print("所有转码已完成！")
+print("\nall transcode complete\n")
